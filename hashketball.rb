72d10aa692ba14ecf2a76667cf4c126992e0cfd7
Sara Walker <sarawall1187@gmail.com>
2018-12-28 20:38:31 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,19 @@
-# Write your code here!
+def game_hash = {
+  :home => {
+    :team_name =>" "
+    :colors => []
+    :player => {
+      
+    }
+  }
+  :away => {
+    :team_name => " "
+    :colors => []
+    :player => {
+      
+    }
+  }
+}
 
 
 
